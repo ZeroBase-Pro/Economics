37699572a8551605878d5209cf9170dc6a002fac
bcYng-w <YikL-10_14@outlook.com>
2025-07-09 14:09:48 +0800

    init

---
+++ src/ZeroBase.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.22;
+ 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {OFT} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFT.sol";
+ 
+contract ZEROBASE is OFT {
+    uint256 public constant INITIAL_SUPPLY = 1000000000 * 10 ** 18;
+    constructor(
+        address _lzEndpoint,
+        address _owner
+    ) OFT("ZEROBASE", "ZB", _lzEndpoint, _owner) Ownable(_owner) {
+        _mint(_owner, INITIAL_SUPPLY);
+    }
+}
