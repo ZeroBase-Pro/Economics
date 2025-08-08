07f0e015724e63767026d4a76426f93142cdb7d1
bcYng-w <YikL-10_14@outlook.com>
2025-08-08 12:19:17 +0800

    fixed

--- script/ZeroBase.s.sol
+++ script/ZeroBase.s.sol
@@ -1,17 +1,18 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
  
 import {Script} from "../lib/forge-std/src/Script.sol";
 import {console2} from "../lib/forge-std/src/console2.sol";
-import "../src/ZeroBase.sol";
+import {ZEROBASE} from "../src/ZeroBase.sol";
  
 contract ZeroBaseScript is Script {
-    address constant LAYERZERO_ENDPOINT = 0x6EDCE65403992e310A62460808c4b910D972f10f;
     // address constant LAYERZERO_ENDPOINT = 0x6EDCE65403992e310A62460808c4b910D972f10f;
+    address constant LAYERZERO_ENDPOINT = 0x1a44076050125825900e736c501f859c50fE728c; //use this when deploy to arb/bsc mainnet
 
     function run() public {
         // Setup
-        uint256 privateKey = vm.envUint("PRIVATE_KEY");
+        // uint256 privateKey = vm.envUint("PRIVATE_KEY");
+        uint256 privateKey = vm.envUint("PRIVATE_KEY_MAIN");
         vm.startBroadcast(privateKey);
 
         // Deploy
@@ -21,5 +22,3 @@ contract ZeroBaseScript is Script {
         vm.stopBroadcast();
     }
 }
-//forge script script/ZeroBase.s.sol:ZeroBaseScript --rpc-url https://eth-sepolia.api.onfinality.io/public --broadcast --etherscan-api-key F41MZG297XBH3D4RHMN96Y6S15HYFDJQNC --verify
-//forge script script/ZeroBase.s.sol:ZeroBaseScript --rpc-url https://bsc-testnet.public.blastapi.io --broadcast --etherscan-api-key XUXXIDDRA5UMHVVXFUSKDFUGRXVM5ZX2E2 --verify
---
+++ test/ZEROBASE.t.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import {Test} from "forge-std/Test.sol";
+import {ZEROBASE} from "../src/ZEROBASE.sol";
+
+contract A{}
+
+contract ZEROBASETest is Test {
+    ZEROBASE token;
+    address lzEndpoint = 0x6EDCE65403992e310A62460808c4b910D972f10f; // Mock endpoint
+    address owner = address(0x2222);
+    address receiver = address(0x3333);
+
+    function setUp() public {
+        vm.createSelectFork(vm.envString("SEPOLIA_RPC_URL"));
+        token = new ZEROBASE(lzEndpoint, owner, receiver);
+    }
+
+    function testNameAndSymbol() public view {
+        assertEq(token.name(), "ZEROBASE Token");
+        assertEq(token.symbol(), "ZBT");
+    }
+
+    function testInitialSupplyMintedToReceiver() public view {
+        uint256 expectedSupply = 1_000_000_000 * 10 ** 18;
+        assertEq(token.totalSupply(), expectedSupply);
+        assertEq(token.balanceOf(receiver), expectedSupply);
+    }
+
+    function testOwnerIsCorrect() public view {
+        assertEq(token.owner(), owner);
+    }
+
+    function testTransferWorks() public {
+        vm.prank(receiver);
+        require(token.transfer(address(0x4444), 1000 ether));
+        assertEq(token.balanceOf(address(0x4444)), 1000 ether);
+    }
+
+    function testOnlyOwnerCanDoOwnerStuff() public {
+        vm.startPrank(address(0x9999));
+        vm.expectRevert();
+        token.transferOwnership(address(0x1234));
+        vm.stopPrank();
+
+        vm.prank(owner);
+        token.transferOwnership(address(0x1234));
+        assertEq(token.owner(), address(0x1234));
+    }
+}
